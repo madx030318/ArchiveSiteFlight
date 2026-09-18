f899849f2c30ba8fc80cbbb9addbf756637834de
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-18 11:26:08 +0500

    Update and rename DataAnalysisPart.ipynb to DataAnalysisPart.py
    Performing the data analysis part for input data to be used for archive

---
+++ DataAnalysisPart.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn import train_test_split
+import matplotlib.pyplot as plt
+
+input_data = pd.read_csv(r"flights.csv")
+input_data["Year"] = input_data["Year"].replace(2008, 2026)
+
+
+input_data["DepTime"] = pd.to_numeric(input_data["DepTime"], errors = "coerce")
+
+input_data["DepTime"] = df["DepTime"].apply( lambda x: f"{int(x):04d}" if pd.notna(x) else None)
+
+input_data["DepTime"] = pd.to_datetime(input_data["DepTime"], format="%H%M", errors="coerce").dt.time
+
+input_data["CRSDepTime"] = pd.to_numeric( df["CRSDepTime"], errors="coerce")
+
+input_data["CRSDepHour"] = ( input_data["CRSDepTime"] // 100)
+
+
+input_data["ArrTime"] = pd.to_numeric(df["ArrTime"], errors="coerce")
+
+input_data["ArrTimeF"] = input_data["ArrTime"].apply(lambda x: f"{int(x):04d}" if pd.notna(x) else None)
+
+input_data["ArrTimeF"] = pd.to_datetime(input_data["ArrTimeF"], format="%H%M", errors="coerce").dt.strftime("%H:%M")
+
+
+
+input_data["CarrierDelay"] = pd.to_numeric(input_data["CarrierDelay"], errors="coerce")
+
+input_data["CarrierDelayClean"] = input_data["CarrierDelay"].fillna(0)
+
