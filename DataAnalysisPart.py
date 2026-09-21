cc29cd077955100ace572682b62c75a562022849
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-22 02:07:15 +0500

    Update DataAnalysisPart.py
    the necessary actions were marked as comment

--- DataAnalysisPart.py
+++ DataAnalysisPart.py
@@ -41,6 +41,60 @@ def categorize_delay(x):
     else:
         return "Long Delay"
 
-input_data["CarrierDelayCategory"] = input_data["CarrierDelayClean"].apply(
-    categorize_delay
-)
+input_data["CarrierDelayCategory"] = input_data["CarrierDelayClean"].apply(categorize_delay)
+
+#Second type of delays - weather delay
+
+input_data["WeatherDelay"] = pd.to_numeric(input_data["WeatherDelay"], errors="coerce")
+
+input_data["WeatherDelayClean"] = input_data["WeatherDelay"].fillna(0)
+
+
+def weather_delay(x):
+    if x == 0:
+        return "No Delay"
+    elif x <= 15:
+        return "Short Delay"
+    elif x <= 60:
+        return "Moderate Delay"
+    else:
+        return "Long Delay"
+
+input_data["WeatherDelayCategory"] = input_data["WeatherDelayClean"].apply(weather_delay)
+
+#third delay of air-flights type - NasDelay 
+
+input_data["NASDelay"] = pd.to_numeric(input_data["NASDelay"], errors="coerce")
+
+input_data["NASDelayClean"] = input_data["NASDelay"].fillna(0)
+
+
+def nas_delay(x):
+    if x == 0:
+        return "No Delay"
+    elif x <= 15:
+        return "Short Delay"
+    elif x <= 60:
+        return "Moderate Delay"
+    else:
+        return "Long Delay"
+
+input_data["WeatherDelayCategory"] = input_data["WeatherDelayClean"].apply(nas_delay)
+
+
+# Automation process to dynamically the data will be analyzed through the cycle dpeend of column"s data type. Integration with c++ backend
+
+archive_data_f = input_data[[]];
+
+archive_data_f.to_json("archive_formatted.json", oriented="records", indent = 4)
+
+
+
+
+
+
+
+
+
+
+
